7b1d60e4d8d6694f3d27a0d1478b1d8b2a35373f
Ruslan Donchenko <ruslan_donchenko@list.ru>
2020-04-21 10:51:47 +0300

    tootipster styles

--- dev/app/modules/register.js
+++ dev/app/modules/register.js
@@ -10,13 +10,19 @@
       if($(error).text() == '') {
         return
       }
+      var tooltipsterContent = `
+        <p class="tooltipster-content__caption">Error name here</p>
+        <p class="tooltipster-content__desctiption">Validation error description here</p>
+      `;
+      $(element).removeClass('validation-success');
       $(element).next().next('.error-icon').removeClass('d-none');
-      $(element).next().next('.error-icon').tooltipster("content", $(error).text());
+      $(element).next().next('.error-icon').tooltipster("content", tooltipsterContent);
       $(element).next().next('.error-icon').tooltipster("open");
     },
     success(label, element) {
       $(element).next().next('.error-icon').addClass('d-none');
       $(element).next().next('.error-icon').tooltipster("close");
+      $(element).addClass('validation-success');
     },
     rules: {
       username: {
@@ -39,6 +45,29 @@
     onlyOne: false,
     position: 'top',
     theme: ['tooltipster-noir', 'tooltipster-noir-customized'],
+    functionPosition: function(instance, helper, position){
+      if(window.innerWidth < 460) {
+        position.coord.left += 70;
+      }
+      if(window.innerWidth < 770) {
+        position.coord.left -= 84;
+      }
+      else if(window.innerWidth < 1030) {
+        position.coord.left -= 60;
+      }
+      else if(window.innerWidth < 1300) {
+        position.coord.left -= 75;
+      }       
+      else if(window.innerWidth < 1370) {
+        position.coord.left -= 80;
+      } 
+      else {
+        position.coord.left -= 84;
+      }
+      
+      return position;
+    },
+    contentAsHTML: true,
   });
 
   $(".form-group input").on("focusout", event => {
