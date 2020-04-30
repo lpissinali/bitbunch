c3355110eaf525aa8f80162efc2b4ea0467b8350
Ruslan Donchenko <ruslan_donchenko@list.ru>
2020-04-30 18:37:35 +0300

    feat: new accordion draft

--- dev/app/main.js
+++ dev/app/main.js
@@ -1,8 +1,7 @@
 import '../common/scss/main.scss';
 
 // accordion
-import { dsAccordion } from '../components/accordion/ds-accordion';
-
+import Accordion from '../components/accordion/ds-accordion-gsap';
 require('./polyfills/polyfills');
 require('./libs/bootstrap-select/bootstrap-select.min.js');
 require('./modules/validators');
@@ -15,12 +14,12 @@ require('./modules/contact');
 require('./modules/masks');
 
 // аккордион faq
-const faqAccordion = new dsAccordion({});
+const faqAccordion = new Accordion({});
 
-const careerAccordion = new dsAccordion({
-  mainContainer: '.career__accordion',
-  dropdownBlock: '.career__acd-dropdown',
-});
+// const careerAccordion = new Accordion({
+//   mainContainer: '.career__accordion',
+//   dropdownBlock: '.career__acd-dropdown',
+// });
 
 // анимация
 require('./page-home-anim');
