24de077416fe116d98a0001e08df485a5e3a372a
Ruslan Donchenko <ruslan_donchenko@list.ru>
2020-05-18 07:55:41 +0300

    refactoring: form and validation

--- dev/app/modules/forgot.js
+++ dev/app/modules/forgot.js
@@ -1,77 +1,29 @@
-/* eslint-disable func-names */
-/**
- * @module forgotForm
- * @author donrus
- * @description validation and form submit
- */
-(function($) {
-  $("#forgotForm").validate({
-    errorPlacement(error, element) {
-      if($(error).text() == '') {
-        return
-      }
-      var tooltipsterContent = `
-        <p class="tooltipster-content__caption">This field is obligatory</p>
-        <p class="tooltipster-content__description">Please fill it in to continue</p>
-      `;
-      $(element).removeClass('validation-success');
-      $(element).next().next('.error-icon').removeClass('d-none');
-      $(element).next().next('.error-icon').tooltipster("content", tooltipsterContent);
-      $(element).next().next('.error-icon').tooltipster("open");
-    },
-    success(label, element) {
-      $(element).next().next('.error-icon').addClass('d-none');
-      $(element).next().next('.error-icon').tooltipster("close");
-      $(element).addClass('validation-success');
-    },
-    rules: {
-      email: {
-        required: true
-      },
-      password: {
-        required: true
-      }
-    },
-    errorClass: "validation-error",
-    submitHandler(form) { return false;}
-  });
+import {
+  VALIDATION_SUCCESS_CLASS,
+  VALIDATION_ERROR_CLASS,
+  showTooltip,
+  closeTooltip,
+} from './commonForm';
 
-  // initialize tooltipster on text input elements
-  $('#forgotForm .error-icon').tooltipster({
-    trigger: 'custom',
-    onlyOne: false,
-    position: 'top',
-    theme: ['tooltipster-noir', 'tooltipster-noir-customized'],
-    functionPosition: function(instance, helper, position){
-      if(window.innerWidth < 460) {
-        position.coord.left += 45;
-      }
-      if(window.innerWidth < 770) {
-        position.coord.left -= 60;
-      }
-      else if(window.innerWidth < 1030) {
-        position.coord.left -= 43;
-      }
-      else if(window.innerWidth < 1300) {
-        position.coord.left -= 53;
-      }       
-      else if(window.innerWidth < 1370) {
-        position.coord.left -= 56;
-      } 
-      else {
-        position.coord.left -= 60;
-      }
-      
-      return position;
+$('#forgotForm').validate({
+  errorPlacement(error, element) {
+    if ($(error).text() === '') return;
+    showTooltip(element, $(error).text());
+  },
+  success(label, element) {
+    closeTooltip(element);
+  },
+  rules: {
+    email: {
+      required: true,
     },
-    contentAsHTML: true,
-  });
-
-  $(".form-group input").on("focusout", event => {
-    if (event.currentTarget.value !== "") {
-      $(event.currentTarget).next().addClass("has-content");
-    } else {
-      $(event.currentTarget).next().removeClass("has-content");
-    }
-  });
-})(jQuery);
+    password: {
+      required: true,
+    },
+  },
+  errorClass: VALIDATION_ERROR_CLASS,
+  validClass: VALIDATION_SUCCESS_CLASS,
+  submitHandler(form) {
+    return false;
+  },
+});
